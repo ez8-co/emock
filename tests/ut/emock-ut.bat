6103894429a673d9366eb99b5fd511a09c8f794e
Orca <orca.zhang@yahoo.com>
2022-08-09 18:59:52 +0800

    :panda_face: try to fix for vs2019

--- tests/ut/emock-ut.bat
+++ tests/ut/emock-ut.bat
@@ -7,4 +7,4 @@ for /r %%i in (Debug\emock-ut-*.dll) do (
    set cases=!cases! %%~pni
 )
 @echo !cases!
-testngpp-runner.exe -L. -l"testngppxmllistener result.xml" -l"testngppstdoutlistener -c " -c100 !cases!
+runas /user:administrator testngpp-runner.exe -L. -l"testngppxmllistener result.xml" -l"testngppstdoutlistener -c " -c100 !cases!
